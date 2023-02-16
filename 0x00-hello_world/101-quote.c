3407aa4fcd8c3c178f567109046ad7477feffc86
alx-pre_course <3aba9irablog@gmail.com>
2023-02-16 11:56:41 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 #include <stdio.h>
-
+#include <string.h>
 /**
  * main - prints a quote
  * Return: 1
  */
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(STDOUT_FILENO, quote, strlen(quote));
 	return (1);
