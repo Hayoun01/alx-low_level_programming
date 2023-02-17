49e730a1a6b60cfe67bbdf9ca403d9ac7983ae4b
alx-pre_course <3aba9irablog@gmail.com>
2023-02-17 06:41:46 -0800

    print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,7 +2,8 @@
 
 /**
  * main - Entry point
- * Description: It prints letters using just
+ * Description: It prints the alphabet in lowercase
+ *		following by new line using just
  *		`putchar` and ASCII numbers.
  * Return: Always 0 (success)
  */
