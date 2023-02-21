05b0b070f9d46bc26572c03d6cb0dcae362f5328
alx-pre_course <3aba9irablog@gmail.com>
2023-02-21 12:59:48 -0800

    24_hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day.
+ *
+ * Return: the last digit.
+ */
+void jack_bauer(void)
+{
+	int start = 0, stop = 24;
+	int min;
+
+	while (start < stop)
+	{
+		if (start < 10)
+			_putchar('0');
+		for (min = 0; min < 60; min++)
+		{
+			_putchar('0' + start);
+			_putchar(':');
+			if (min < 10)
+				_putchar('0');
+			_putchar('0' + min);
+		}
+	}
+}
