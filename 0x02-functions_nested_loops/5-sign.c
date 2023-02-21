2bb4c2454e16db0a6663fa970f4433c93f041dd6
alx-pre_course <3aba9irablog@gmail.com>
2023-02-21 10:12:49 -0800

    print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_sig - Checks sign of a num.
+ * @n: take number.
+ * Return: 1 if is alpha 0 otherwise
+ */
+int print_sign(int n)
+{
+	if (c > 0)
+		_putchar('+');
+		return (1);
+	else if (c < 0)
+		_putchar('-');
+		return (-1);
+	_putchar('0')
+	return (0);
+}
