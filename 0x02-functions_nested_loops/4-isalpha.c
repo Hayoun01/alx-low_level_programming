88d80ab9cbbf386859730ce23b1e897623f2c3d7
alx-pre_course <3aba9irablog@gmail.com>
2023-02-21 10:00:13 -0800

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks if a character is alpha
+ * @c: take one character.
+ * Return: 1 if is alpha 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}
