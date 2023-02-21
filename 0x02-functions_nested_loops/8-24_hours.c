e5b53d39289ced302ff5167dbf2632d02fda4564
alx-pre_course <3aba9irablog@gmail.com>
2023-02-21 13:26:07 -0800

    24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,12 +11,14 @@ void jack_bauer(void)
 
 	for (a = 0; a <= 2; a++)
 	{
-		for (b = 0; b <= 3; b++)
+		for (b = 0; b <= 9; b++)
 		{
 			for (c = 0; c <= 5; c++)
 			{
 				for (d = 0; d <= 9; d++)
 				{
+					if (a == 2 && b > 3)
+						break;
 					_putchar('0' + a);
 					_putchar('0' + b);
 					_putchar(':');
