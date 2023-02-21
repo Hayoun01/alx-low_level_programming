bba0ba2d763d2572cfe2e2dce2505052f95c663e
alx-pre_course <3aba9irablog@gmail.com>
2023-02-21 14:03:29 -0800

    times_tables

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - prints times table.
+ *
+ * Return: Nothing.
+ */
+void times_table(void)
+{
+	int m,n, o;
+
+	for (m = 0; m <= 9; m++)
+	{
+		_putchar('0');
+		for (n = 1; n <= 9; n++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			o = m * n;
+			if (o <= 9)
+			{
+				_putchar(' ');
+				_putchar('0' + o);
+			}
+			else
+			{
+				_putchar('0' + (o / 10));
+				_putchar('0' + (o %10));
+			}
+		}
+		_putchar('\n');
+	}
+
+}
