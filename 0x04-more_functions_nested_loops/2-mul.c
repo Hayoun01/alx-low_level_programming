1a33c600b97a3f04be95ed40f9d1299428531281
alx-pre_course <3aba9irablog@gmail.com>
2023-02-23 11:57:45 -0800

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Check if the char is number.
+ *
+ * @a: 1st digit.
+ * @b: 2nd digit.
+ *
+ * Return: multiplies two digits.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
