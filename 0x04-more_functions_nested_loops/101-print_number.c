9bfc1a04d34f5c206853e6237be270cf43a6b34a
alx-pre_course <3aba9irablog@gmail.com>
2023-02-23 15:57:23 -0800

    print_number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,7 +8,7 @@
  * Return: Nothing
  */
 void print_number(int n)
-i{
+{
 	unsigned int dig = n;
 
 	if (n < 0)
@@ -18,8 +18,8 @@ i{
 	}
 	if (n / 10 != 0)
 	{
-		print_number(n / 10);
+		print_number(dig / 10);
 	}
-	_putchar(n % 10 + '0');
+	_putchar((dig % 10) + 48);
 }
 
