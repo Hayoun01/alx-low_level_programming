bc885af53f99eff265b8f4209e0d4d0a3a78c922
alx-pre_course <3aba9irablog@gmail.com>
2023-02-23 14:45:18 -0800

    Fizz-Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,14 +12,18 @@ int main(void)
 	int num;
 
 	for (num = 1; num <= 100; num++)
+	{
 		if (num % 5 == 0 && num % 3 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (num % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if (num % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else
-			printf("%d ", num);
+			printf("%d", num);
+		if (num != 100)
+			printf(" ");
+	}
 	printf("\n");
 	return (0);
 }
