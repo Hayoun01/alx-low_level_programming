ac7d0e650cc0a1ed5c509d11dce09df43239add7
alx-pre_course <3aba9irablog@gmail.com>
2023-02-27 15:53:23 -0800

    100-atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,25 @@
+/**
+ * _atoi - Converts a string to an integer.
+ *
+ * @s: The string to convert.
+ *
+ * Return: The integer value of the string.
+ */
+int _atoi(char *s)
+{
+	int sign = 1, result = 0;
+	char c;
+
+	while (*s != '\0')
+	{
+		c = *s++;
+		if (c == '-')
+			sign = -1;
+		else if (c >= '0' && c <= '9')
+			result = result * 10 + (c - '0');
+		else if (result != 0)
+			break;
+	}
+	return (result * sign);
+}
+
