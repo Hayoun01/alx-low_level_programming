6fc54ad8e0c605b2c97dc59a12a58bfeace7db31
alx-pre_course <3aba9irablog@gmail.com>
2023-03-02 03:50:11 -0800

    cap first letter

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,7 +15,7 @@ char *cap_string(char *s)
 	for (i = 0; s[i]; i++)
 	{
 		if (s[i] >= 'a' && s[i] <= 'z' && i == 0)
-			s[i + 1] -= 32;
+			s[i] -= 32;
 		for (j = 0; sym[j]; j++)
 			if (s[i] == sym[j])
 				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
