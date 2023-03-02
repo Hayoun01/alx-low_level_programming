4fd745310f2098ad3ae8bdd5939e8e4b65d33a3c
alx-pre_course <3aba9irablog@gmail.com>
2023-03-02 07:54:28 -0800

    rot13

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+  
 	int _putchar(char);
 	char *_strcat(char *dest, char *src);
 	char *_strncat(char *dest, char *src, int n);
@@ -10,5 +10,6 @@
 	char *string_toupper(char *);
 	char *cap_string(char *s);
 	char *leet(char *s);
+	char *rot13(char *);
 
 #endif
