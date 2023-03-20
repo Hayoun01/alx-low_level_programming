35c3dc15efec0664e74f1b47187da6e8ae0f751c
alx-pre_course <3aba9irablog@gmail.com>
2023-03-20 12:56:58 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,19 @@
-#ifndef DOG_H
-#define DOG_h
+#ifndef DOG
+#define DOG
 
-typedef struct dog {
+/**
+ * struct dog - dog type
+ *
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
+ */
+struct dog {
 	char   *name;
 	float  age;
 	char   *owner;
-} dog;
+};
 
 #endif
