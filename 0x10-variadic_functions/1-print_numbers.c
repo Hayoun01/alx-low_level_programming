d74763e6ec3e05851dd037d15f6301f417c8fe67
alx-pre_course <3aba9irablog@gmail.com>
2023-03-23 07:22:14 -0700

    print_numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,10 +11,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ptr;
 	unsigned int i;
 
-	va_start(ptr, n);
-
-	for (i = 0; i < n - 1; i++)
-		printf("%d%s", va_arg(ptr, int), separator);
-	printf("%d\n", va_arg(ptr, int));
-	va_end(ptr);
+	if (n != 0)	
+		va_start(ptr, n);
+		for (i = 0; i < n - 1; i++)
+			printf("%d%s", va_arg(ptr, int), separator);
+		printf("%d\n", va_arg(ptr, int));
+		va_end(ptr);
 }
