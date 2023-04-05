8d2f3eb7f9e2c3c93ddcf5237069b371bd038597
alx-pre_course <3aba9irablog@gmail.com>
2023-04-04 18:36:52 -0700

    print_listint_safe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,9 +1,9 @@
 #include "lists.h"
+#include <stdlib.h>
 #include <stdio.h>
 
 /**
- * re - reallocates memory for an array of pointers
- *
+ * _r - reallocates memory for an array of pointers
  * to the nodes in a linked list
  * @list: the old list to append
  * @size: size of the new list (always one more than the old list)
@@ -11,13 +11,13 @@
  *
  * Return: pointer to the new list
  */
-const listint_t **re(const listint_t **list, size_t size, const listint_t *new)
+const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
 {
 	const listint_t **newlist;
 	size_t i;
 
 	newlist = malloc(size * sizeof(listint_t *));
-	if (!newlist)
+	if (newlist == NULL)
 	{
 		free(list);
 		exit(98);
@@ -40,7 +40,7 @@ size_t print_listint_safe(const listint_t *head)
 	size_t i, num = 0;
 	const listint_t **list = NULL;
 
-	while (head)
+	while (head != NULL)
 	{
 		for (i = 0; i < num; i++)
 		{
@@ -52,11 +52,10 @@ size_t print_listint_safe(const listint_t *head)
 			}
 		}
 		num++;
-		list = re(list, num, head);
+		list = _r(list, num, head);
 		printf("[%p] %d\n", (void *)head, head->n);
 		head = head->next;
 	}
 	free(list);
 	return (num);
 }
-
