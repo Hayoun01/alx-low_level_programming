1542a40d6944923003a802d9188aa5956172c9cb
Mohammed Hayyoun <3aba9irablog@gmail.com>
2023-04-07 14:41:19 +0000

    binary_to_uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,19 +9,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    int len = strlen(b) - 1, base2;
-    unsigned int result = 0;
+	int len = strlen(b) - 1, base2;
+	unsigned int result = 0;
 
-    for (base2 = 1; len >= 0; len--, base2 *= 2)
-    {
-        if (b[len] != '1' && b[len] != '0')
-            return (0);
-        /* '1' & 1 = 49 & 1 = 0011 0001 & 0000 0001 = 0000 0001 = 1 */
-        /* '0' & 1 = 48 & 1 = 0011 0000 & 0000 0001 = 0000 0000 = 0 */
-        else if (b[len] & 1)
-        {
-            result += base2;
-        }
-    }
-    return (result);
+	for (base2 = 1; len >= 0; len--, base2 *= 2)
+	{
+		if (b[len] != '1' && b[len] != '0')
+			return (0);
+		/* '1' & 1 = 49 & 1 = 0011 0001 & 0000 0001 = 0000 0001 = 1 */
+		/* '0' & 1 = 48 & 1 = 0011 0000 & 0000 0001 = 0000 0000 = 0 */
+		else if (b[len] & 1)
+		{
+			result += base2;
+		}
+	}
+	return (result);
 }
