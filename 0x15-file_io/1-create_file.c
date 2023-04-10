8fe935d8d8cefe2cfa76980fc0d806d23a080b3f
Mohammed Hayyoun <3aba9irablog@gmail.com>
2023-04-10 18:39:42 +0000

    create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,19 +9,19 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int f, r;
+	int f, r;
 
-    if (filename == NULL)
-        return (-1);
-    f = open(filename, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR);
-    if (f == -1)
-        return (-1);
-    if (text_content != NULL)
-    {
-        r = write(f, text_content, strlen(text_content));
-        if (r == -1)
-            return (-1);
-    }
-    close(f);
-    return (1);
+	if (filename == NULL)
+		return (-1);
+	f = open(filename, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR);
+	if (f == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		r = write(f, text_content, strlen(text_content));
+		if (r == -1)
+			return (-1);
+	}
+	close(f);
+	return (1);
 }
